c3d688f0610e583522fdaf421c9a86b6d9b7d1f3
Jose Leonardo Osorio Rios <josorior@Joses-MacBook-Pro.local>
2022-11-11 09:36:50 -0500

    info

---
+++ app/singleton_apirest/Controllers/server.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace singleton_apirest.Controllers
+{
+    [ApiController]
+    [Route("server")]
+    public class server : ControllerBase
+    {
+        [HttpGet][Route("hello")]
+        public dynamic hello()
+        {
+            return new
+            {
+                msg = Singleton.Instance.mensaje
+            };
+        }
+        [HttpGet]
+        [Route("time")]
+        public dynamic time()
+        {
+            return new
+            {
+                datetime = Singleton.time
+            };
+        }
+        [HttpPost]
+        [Route("dirlist")]
+        public dynamic dirlist([FromBody] string dirpath)
+        {
+            try
+            {
+                return new
+                {
+                    files = Singleton.dirlist(dirpath)
+                };
+            }
+            catch (Exception e)
+            {
+                return new
+                {
+                    msg = e
+                };
+            }
+        }
+        [HttpGet]
+        [Route("index")]
+        public ContentResult index()
+        {
+            var html = System.IO.File.ReadAllText(@"./assets/index.html");
+            html = html.Replace("{{saludo}}", Singleton.Instance.mensaje);
+            html = html.Replace("{{fecha}}", Singleton.time);
+            return base.Content(html, "text/html");
+        }
+    }
+}
---
+++ app/singleton_apirest/Singleton.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using static System.Net.WebRequestMethods;
+
+namespace singleton_apirest
+{
+    public class Singleton
+    {
+        private static Singleton instance = null;
+        public string mensaje = "";
+
+        protected Singleton()
+        {
+            mensaje = "Hi";
+        }
+
+        public static String time
+        {
+            get
+            {
+                return System.DateTime.UtcNow.ToString();
+            }
+        }
+
+        public static List<Dictionary<string, object>> dirlist(String dirpath)
+        {
+            List<Dictionary<string, object>> files_list = new List<Dictionary<string, object>>();
+            Dictionary<string, object> files_dir = new Dictionary<string, object>();
+            try
+            {
+                DirectoryInfo d = new DirectoryInfo(@dirpath);
+
+                FileInfo[] Files = d.GetFiles("*"); //Getting Text files
+
+                int i = 0;
+                foreach (FileInfo file in Files)
+                {
+                    i++;
+                    files_dir.Add(i.ToString(), file.Name);
+                }
+                files_list.Add(files_dir);
+            }
+            catch (Exception e)
+            {
+                files_dir.Add("msg", e);
+                files_list.Add(files_dir);
+            }
+            return files_list;
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+
+                return instance;
+            }
+        }
+
+    }
+}
